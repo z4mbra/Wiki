9d3c65cbc12b5e6ce053f8cd093f1c0c36cb34b9
z4mbra <gabrielzambrana15@outlook.com>
2024-01-09 16:26:09 -0300

    Tudo certo

--- Cadastro_Estudo/config.php
+++ Cadastro_Estudo/config.php
@@ -8,12 +8,13 @@ $database = "carros";
 $conn = new mysqli($servername, $username, $password, $database);
 
 $nome = $_POST['nome'];
+$rate = $_POST['rate'];
 
 if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO carro(nome) VALUE('$nome')";
+$sql = "INSERT INTO carro(nome,raridade) VALUE('$nome','$rate')";
 $result = $conn->query($sql);
 
 
--- Cadastro_Funcionando/config.php
+++ Cadastro_Funcionando/config.php
@@ -1,11 +1,22 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "carros";
 
 
-    $Host = 'localhost';
-    $Name = 'carros';
-    $Username = 'root';
-    $Password = '';
+$conn = new mysqli($servername, $username, $password, $database);
 
-    $con = new mysqli($Host, $Username, $Password, $Name);
+$nome = $_POST['nome'];
+$rate = $_POST['rate'];
 
-    ?>
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO carro(nome,raridade) VALUE('$nome','$rate')";
+$result = $conn->query($sql);
+
+
+$conn->close();
+?>
